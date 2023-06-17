a073a6e6028a1fcbb787e331d027fcabacf9b732
stirelshka8 <stirelshka8@gmail.com>
2023-06-17 08:35:13 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,56 @@
+import os
+import openai
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NUMBERS_ROWS = 6
+openai.api_key = os.getenv("AI_TOKEN")
+bot = telebot.TeleBot(os.getenv("TG_TOKEN"))
+
+if not os.path.exists("users"):
+    os.mkdir("users")
+
+
+@bot.message_handler(content_types=['text'])
+def msg(message):
+    if f"{message.chat.id}.txt" not in os.listdir('users'):
+        with open(f"users/{message.chat.id}.txt", "x") as f:
+            f.write('')
+
+    with open(f'users/{message.chat.id}.txt', 'r', encoding='utf-8') as file:
+        oldmes = file.read()
+
+    if message.text == '/clear':
+        with open(f'users/{message.chat.id}.txt', 'w', encoding='utf-8') as file:
+            file.write('')
+        return bot.send_message(chat_id=message.chat.id, text='История очищена!')
+
+    try:
+        send_message = bot.send_message(chat_id=message.chat.id, text='Обрабатываю запрос, пожалуйста подождите!')
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0301",
+            messages=[{"role": "user", "content": oldmes},
+                      {"role": "user", "content": f'Предыдущие сообщения: {oldmes}; Запрос: {message.text}'}],
+            presence_penalty=0.6)
+
+        bot.edit_message_text(text=completion.choices[0].message["content"], chat_id=message.chat.id,
+                              message_id=send_message.message_id)
+
+        with open(f'users/{message.chat.id}.txt', 'a+', encoding='utf-8') as file:
+            file.write(message.text.replace('\n', ' ') + '\n' +
+                       completion.choices[0].message["content"].replace('\n', ' ') + '\n')
+
+        with open(f'users/{message.chat.id}.txt', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        if len(lines) >= NUMBERS_ROWS + 1:
+            with open(f'users/{message.chat.id}.txt', 'w', encoding='utf-8') as f:
+                f.writelines(lines[2:])
+
+    except Exception as e:
+        bot.send_message(chat_id=message.chat.id, text=str(e))
+
+
+bot.infinity_polling()
